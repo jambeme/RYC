b8166db4caa08299c8b035e8b5839f7d3c2779f1
jam <me@jamshed.me>
2025-01-19 22:16:33 +0800

    fixed the uuid expiry date

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ u = controller.get('uuid')
 
 if u is None:
     u = str(uuid.uuid4())
-    controller.set('uuid', u, expires_at = 0)
+    controller.set('uuid', u, expires_at = 2199-12-31)
 
 grade = st.selectbox('Enter your grade:', options = [9, 10, 11, 12])
 major = st.selectbox('Enter the major/field you are most interested in:', options = ['Math', 'Science', 'Comp-Sci', 'Med'])
